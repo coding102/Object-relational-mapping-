6abf4102031a8da6ecbf6c5b9ade0bc414b45d4e
Marck <mercercountynj@gmail.com>
2016-01-23 19:37:52 -0500

    ORM without parsing

--- my.rb
+++ my.rb
@@ -23,10 +23,7 @@ end
 def self.find(id)
     # find - takes an ID argument and finds the User with that ID, returns a user object w/ information on that user from the DB
     puts `psql -d markdatabase -c "SELECT * FROM users WHERE id = #{id}"`
-    # assign to a
-    a = sql_results_parser(command)
-    # call that a 
-    a[0]
+
 end
     
     
@@ -35,8 +32,7 @@ end
 # where - takes a Hash argument of user attributes and finds users with those attributes, returns an array of matching User objects
 def self.where(email)
     puts `psql -d markdatabase -c "SELECT * FROM users WHERE email = '#{email}'"`
-    z = sql_results_parser(command)
-    z[0]
+
 end    
     
     
@@ -46,9 +42,7 @@ def self.all
     #selecting from available users
     puts `psql -d markdatabase -c "SELECT * FROM users"`
     #parse
-    z = sql_results_parser (command)
-    #returns as array
-    z[]
+
 end
     
     
@@ -56,11 +50,8 @@ end
 #last - returns an object containing the last user in the database (continued on the next page) 
 def self.last
     #order by DESC and calling first on the list
-    puts `psql -d markdatabase -c * FROM users ORDER BY user_id DESC LIMIT 1"`
-    #assign to z
-    z = sql_results_parser (command)
-    #call z "first"
-    z.first
+    puts `psql -d markdatabase -c "SELECT * FROM users ORDER BY id DESC LIMIT 1";`
+    
 end
       
 
@@ -68,13 +59,17 @@ end
 #first - returns an object containing the first user in the database 
 def self.first
     #selects first user using enrollment time a limiting to the first on the list
-    puts `psql -d markdatabase -c "SELECT timestamp * FROM users LIMIT 1"`
-    #assign to z and parse
-    z = sql_results_parser (command)
-    #call z with first function
-    z.first
+    puts `psql -d markdatabase -c "SELECT * FROM users ORDER BY id LIMIT 1";`
+    
+
 end
-        
+
+    
+    
+    
+    
+    
+    
 end
 
 
